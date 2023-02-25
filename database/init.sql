c87f84ff49ff60fb18f9bc33a227551a81f3a41d
kamrul1157024 <kamrul1157024@gmail.com>
2023-02-25 20:40:12 +0600

    Add nginx for devlopment

--- database/init.sql
+++ database/init.sql
@@ -1,5 +1,3 @@
 CREATE DATABASE social_media;
-GRANT ALL PRIVILEGES ON DATABASE social_media TO postgres;
 CREATE DATABASE social_media_test;
-GRANT ALL PRIVILEGES ON DATABASE social_media_test TO postgres;
 
