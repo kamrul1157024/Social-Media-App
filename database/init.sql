bffc21de588886a7c269572f3bf8449f4af5e93a
kamrul1157024 <kamrul1157024@gmail.com>
2023-02-25 20:05:27 +0600

    fix database initialization config

---
+++ database/init.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE social_media;
+GRANT ALL PRIVILEGES ON DATABASE social_media TO postgres;
+CREATE DATABASE social_media_test;
+GRANT ALL PRIVILEGES ON DATABASE social_media_test TO postgres;
+
